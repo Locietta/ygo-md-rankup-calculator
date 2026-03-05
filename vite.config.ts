353ebd7fc8b25e2b630c06c3774eb99770c5427d
Locietta <locietta@gmail.com>
2026-03-05 20:40:55 +0800

    cd: avoid vue-tsc -b relying on vite build when on fresh build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import wasmPack from "vite-plugin-wasm-pack";
 import vuetify from "vite-plugin-vuetify";
+import { fileURLToPath, URL } from "node:url";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
-    wasmPack("./wasm"),
     vuetify({ autoImport: true }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: [
+      {
+        find: /^wasm$/,
+        replacement: fileURLToPath(new URL("./wasm/pkg/wasm.js", import.meta.url)),
+      },
+    ],
+  },
   optimizeDeps: {
-    exclude: ["@wasm/wasm"],
+    exclude: ["wasm"],
   },
 });
